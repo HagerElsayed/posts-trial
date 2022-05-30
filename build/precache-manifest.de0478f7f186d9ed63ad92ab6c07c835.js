dc93dfa097c7b5696e81a4b3bb1d96b16954c4c2
Hager Elsayed <Hasanieenh@mobilenoc.mobi>
2022-05-30 17:58:37 +0200

    add build

---
+++ build/precache-manifest.de0478f7f186d9ed63ad92ab6c07c835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18d21bf7db496be9ca19",
+    "url": "/postsTrial/static/css/main.c1b5bf2c.chunk.css"
+  },
+  {
+    "revision": "18d21bf7db496be9ca19",
+    "url": "/postsTrial/static/js/main.18d21bf7.chunk.js"
+  },
+  {
+    "revision": "325ca37fb911515c36cd",
+    "url": "/postsTrial/static/js/1.325ca37f.chunk.js"
+  },
+  {
+    "revision": "c35c17c2b583c617ff36",
+    "url": "/postsTrial/static/js/2.c35c17c2.chunk.js"
+  },
+  {
+    "revision": "2cc7f2288066874244ab",
+    "url": "/postsTrial/static/js/runtime~main.2cc7f228.js"
+  },
+  {
+    "revision": "698a7bedc66f9499b8f65bca351c90a2",
+    "url": "/postsTrial/index.html"
+  }
+];
